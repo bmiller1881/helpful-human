37531cc317344b4ed16e1510cc4dfbb7d2d985d1
Brandon Miller <Brandon@MacBook-Pro.local>
2023-03-11 20:36:48 -0800

    skeleton of webpack bundled, typescript, react/node app

---
+++ server/server.ts
@@ -0,0 +1,33 @@
+import path from 'path';
+import express, { json, urlencoded, Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT || '3000';
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+// serves static files in production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use('/build', express.static(path.join(__dirname, '../build')));
+  app.get('/*', (req, res) => {
+    return res.status(200).sendFile(path.join(__dirname, '../build/index.html'));
+  });
+}
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  const defaultError = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { Error: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultError, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}...`);
+});
+
+module.exports = server;
